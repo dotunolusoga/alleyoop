56f703de4fa9b2f685df98a2a12e0ce11e0d146a
Dotun Olusoga <adedotun.olusoga@gmail.com>
2016-12-16 19:06:14 -0600

    Transactional emails now sending

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,11 +42,11 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: '',
+    address: 'smtp.sendgrid.net',
     port: 587,
     enable_starttls_auto: true,
-    user_name: '',
-    password: '',
-    authentication: 'login'
+    user_name: 'apikey',
+    password: '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',
+    authentication: 'plain'
   }
 end
