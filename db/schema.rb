9ed42e2c6fe58c789a2f4cd7e52f4be3e36a81be
Dotun Olusoga <adedotun.olusoga@gmail.com>
2016-12-23 12:47:38 -0600

    Add User reference to Experience model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161223183412) do
+ActiveRecord::Schema.define(version: 20161223184633) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,7 +43,10 @@ ActiveRecord::Schema.define(version: 20161223183412) do
     t.boolean  "active"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
+    t.integer  "user_id"
   end
+
+  add_index "experiences", ["user_id"], name: "index_experiences_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -73,4 +76,5 @@ ActiveRecord::Schema.define(version: 20161223183412) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "experiences", "users"
 end
