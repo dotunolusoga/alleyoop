d6b8f9ad1033176deed95c9790da33d31ea7ead5
Dotun Olusoga <adedotun.olusoga@gmail.com>
2016-12-23 14:24:01 -0600

    Add validations to Experience model

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,2 +1,19 @@
 class Experience < ActiveRecord::Base
+  belongs_to :user
+
+  validates :experience_type, presence: true
+  validates :experience_title, presence: true
+  validates :tagline, presence: true
+  validates :summary, presence: true
+  validates :about_host, presence: true
+  validates :experience_date, presence: true
+  validates :start_time, presence: true
+  validates :end_time, presence: true
+  validates :location_name, presence: true
+  validates :street, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :zipcode, presence: true
+  validates :capacity, presence: true
+  validates :price, presence: true
 end
