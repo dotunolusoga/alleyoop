23a0b152475e610913a44f78e7f40e4e96f39c98
Dotun Olusoga <adedotun.olusoga@gmail.com>
2017-02-13 15:25:49 -0600

    Add methods and scopes to Conversation & Messages models

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,11 @@
 class Message < ActiveRecord::Base
   belongs_to :conversation
   belongs_to :user
+
+  validates_presence_of :content, :conversation_id, :user_id
+
+  def message_timestamp
+    created_at.strftime("%v")
+  end
+
 end
